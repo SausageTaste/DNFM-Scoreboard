6d037b09cf9d4f292b92fc81cc6350cf68f3c00f
Sungmin Woo <woos8899@gmail.com>
2022-04-17 08:40:02 +0900

    More precise refining

--- refine_input_images.py
+++ refine_input_images.py
@@ -1,9 +1,10 @@
 import os
 
 from PIL import Image
+import matplotlib.pyplot as plt
 
 
-SRC_IMAGE_FOLDER_PATH = r"C:\Users\woos8\Videos\DNFM\아수라 2022.04.17"
+SRC_IMAGE_FOLDER_PATH = r"C:\Users\woos8\Videos\DNFM\엘마 2022.04.17"
 
 TOP_CROP_Y = 0.25
 BOTTOM_CROP_Y = 0.77
@@ -22,6 +23,52 @@ def __crop_ranking_area(image: Image):
     ))
 
 
+def __iter_image_average_colors_in_column(image: Image, x_coord: int):
+    for y in range(image.height):
+        pixel = image.getpixel((x_coord, y))
+        assert 3 == len(pixel)
+        yield sum(pixel) / len(pixel)
+
+
+def __find_min_max(iterator: iter):
+    item_list = list(iterator)
+    return min(item_list), max(item_list)
+
+
+def __find_cut_points(image: Image):
+    x_coord = image.width * 1019 / 1072
+    average_colors = list(__iter_image_average_colors_in_column(image, x_coord))
+    min_average_color, max_average_color = __find_min_max(average_colors)
+    mid_average_color = (min_average_color + max_average_color) * 0.5
+
+    y_points_higher_than_mid = []
+    for y, average_color in enumerate(average_colors):
+        if average_color > mid_average_color:
+            y_points_higher_than_mid.append(y)
+
+    point_distances = []
+    for i in range(len(y_points_higher_than_mid) - 1):
+        point_distances.append(y_points_higher_than_mid[i + 1] - y_points_higher_than_mid[i])
+
+    mid_distance = sum(__find_min_max(point_distances)) / 2
+    discrete_points = []
+    for i, point_dist in enumerate(point_distances):
+        if point_dist > mid_distance:
+            discrete_points.append(i + 1)
+    discrete_points.insert(0, 0)
+    discrete_points.append(None)
+
+    cut_y_points = []
+    for i in range(len(discrete_points) - 1):
+        index_from = discrete_points[i]
+        index_to = discrete_points[i + 1]
+        adjacent_y_points = y_points_higher_than_mid[index_from:index_to]
+        average_y_point = sum(adjacent_y_points) / len(adjacent_y_points)
+        cut_y_points.append(round(average_y_point))
+
+    return cut_y_points
+
+
 def main():
     output_folder_path = os.path.join(SRC_IMAGE_FOLDER_PATH, "refine_output")
     try:
@@ -36,8 +83,36 @@ def main():
 
         image = Image.open(item_path)
         image = __crop_ranking_area(image)
+        # image.save(os.path.join(output_folder_path, f"{index:0>3}.png"), format="png")
 
-        image.save(os.path.join(output_folder_path, f"{index:0>3}.png"), format="png")
+        cut_y_points = __find_cut_points(image)
+        if 0 != cut_y_points[0]:
+            cut_y_points.insert(0, 0)
+        cut_y_points.append(image.height)
+
+        """
+        x_axis = [xx for xx in range(image.height)]
+        y_axis = []
+        for y in range(image.height):
+            pixel = image.getpixel((image.width * 1019 / 1072, y))
+            average_color = sum(pixel) / len(pixel)
+            y_axis.append(average_color)
+
+        plt.cla()
+        plt.plot(x_axis, y_axis, color="blue")
+        for y in cut_y_points:
+            plt.axvline(x=y, color="red")
+        plt.savefig(os.path.join(output_folder_path, f"{index:0>3}_fig.png"))
+        """
+
+        for i in range(len(cut_y_points) - 1):
+            y_from = cut_y_points[i]
+            y_to = cut_y_points[i + 1]
+            one_banner = image.crop((0, y_from, image.width, y_to))
+            if one_banner.height / image.height < 85 / 418:
+                continue
+            output_path = os.path.join(output_folder_path, f"{index:0>3}_{i:0>3}.png")
+            one_banner.save(output_path, format="png")
 
 
 if "__main__" == __name__:
