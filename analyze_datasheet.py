2e8a01c1a31f512217cca451d5d92e4d09df4e89
Sungmin Woo <woos8899@gmail.com>
2022-04-19 02:46:40 +0900

    Update analyze_datasheet.py

--- analyze_datasheet.py
+++ analyze_datasheet.py
@@ -1,6 +1,10 @@
 import os
 
+import matplotlib.pyplot as plt
+
+
 SRC_ROOT_PATH = r"C:\Users\woos8\Desktop\CSV files"
+OUTPUT_FOL_PATH = r"C:\Users\woos8\Desktop\Analyzed"
 
 
 def __generate_data_from_csv(csv_file_path: str):
@@ -37,34 +41,63 @@ def __calc_sample_variance(sample_data: list, sample_mean: float):
     return summation / (len(sample_data) - 1)
 
 
-def __do_for_one(csv_file_path: str):
-    print(csv_file_path)
+def __make_output_file_name(csv_file_path: str):
+    path = os.path.relpath(csv_file_path, SRC_ROOT_PATH)
+    path, _ = os.path.splitext(path)
+    path = path.replace("\\", "-")
+    path = path.replace("/", "-")
+    return path
 
+
+def __make_pure_score_list(csv_file_path: str):
     score_list = []
     for rank, score in __generate_data_from_csv(csv_file_path):
         if score is not None:
             score_list.append(score)
+    return score_list
 
-    if not __check_ranking_list_validity(score_list):
-        raise RuntimeError(f"Not a valid dataset: {csv_file_path}")
 
-    score_average = sum(score_list) / len(score_list)
-    score_variance = __calc_sample_variance(score_list, score_average)
+def __save_mountain_scatter(csv_file_path, output_file_path, title, score_average):
+    x_axis = []
+    y_axis = []
 
-    print(f"\tmean: {score_average}")
-    print(f"\tvariance: {score_variance}")
+    plt.cla()
+    plt.title(title, fontname="Malgun Gothic")
+    plt.ylim((1850, 2300))
+    plt.axhline(y=score_average, alpha=0.5)
+    for rank, score in __generate_data_from_csv(csv_file_path):
+        if score is None:
+            plt.axvline(x=rank, color="red", alpha=0.5)
+        else:
+            x_axis.append(rank)
+            y_axis.append(score)
+    plt.scatter(x_axis, y_axis, s=1)
+    plt.savefig(output_file_path, dpi=300)
 
 
 def main():
-    for x in os.listdir(SRC_ROOT_PATH):
-        item_path = os.path.join(SRC_ROOT_PATH, x)
+    try:
+        os.mkdir(OUTPUT_FOL_PATH)
+    except FileExistsError:
+        pass
 
+    for item_name_ext in os.listdir(SRC_ROOT_PATH):
+        if not item_name_ext.endswith(".csv"):
+            continue
+
+        item_path = os.path.join(SRC_ROOT_PATH, item_name_ext)
         if not os.path.isfile(item_path):
             continue
-        if not x.endswith(".csv"):
-            continue
 
-        __do_for_one(item_path)
+        item_name, _ = os.path.splitext(item_name_ext)
+        pure_score_list = __make_pure_score_list(item_path)
+        if not __check_ranking_list_validity(pure_score_list):
+            print(f"Not a valid dataset: {item_path}")
+
+        score_average = sum(pure_score_list) / len(pure_score_list)
+        score_variance = __calc_sample_variance(pure_score_list, score_average)
+
+        __save_mountain_scatter(item_path, f'{OUTPUT_FOL_PATH}/scatter_{item_name}.png', item_name, score_average)
 
 
 if __name__ == '__main__':
