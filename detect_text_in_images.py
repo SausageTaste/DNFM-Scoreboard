b9f073df757b7f8c8144669ffce61f61718b895f
Sungmin Woo <woos8899@gmail.com>
2022-04-19 00:03:32 +0900

    Parallelized

--- detect_text_in_images.py
+++ detect_text_in_images.py
@@ -1,6 +1,7 @@
 import os
 import json
 import time
+import multiprocessing as mp
 
 import pytesseract as tes
 
@@ -20,36 +21,57 @@ def __make_output_file_name(fol_path: str):
     return path
 
 
+def __gen_png_files(folder_path: str):
+    for x in os.listdir(folder_path):
+        if x.endswith(".png"):
+            yield x
+
+
+def __gen_work_loc():
+    for loc, folders, files in os.walk(SRC_ROOT_PATH):
+        if not len(files):
+            continue
+
+        yield loc
+
+
+def __do_for_one(loc: str):
+    output_file_path = os.path.join(OUTPUT_FOL_PATH, __make_output_file_name(loc))
+    json_data = []
+    start_time = time.time()
+
+    for file_name_ext in __gen_png_files(loc):
+        file_name, file_ext = os.path.splitext(file_name_ext)
+        file_path = os.path.normpath(os.path.join(loc, file_name_ext))
+        image_content = tes.image_to_string(file_path, lang='ENG', config='--psm 10')
+
+        json_data.append({
+            "file_name": file_name,
+            "file_path": file_path,
+            "image_content": image_content,
+        })
+
+    if not len(json_data):
+        print(f'\tNo png image found')
+        return
+
+    with open(output_file_path, "w", encoding="utf8") as file:
+        json.dump(json_data, file, indent=4)
+    print(f'Done "{output_file_path}" ({time.time() - start_time:.2f} sec)')
+
+
 def main():
     try:
         os.mkdir(OUTPUT_FOL_PATH)
     except FileExistsError:
         pass
 
-    for loc, folders, files in os.walk(SRC_ROOT_PATH):
-        if not len(files):
-            continue
+    start_time = time.time()
+    loc_list = list(__gen_work_loc())
+    with mp.Pool() as p:
+        p.map(__do_for_one, loc_list)
 
-        print(f'Start: "{loc}"')
-        output_file_path = os.path.join(OUTPUT_FOL_PATH, __make_output_file_name(loc))
-        print(f"\tOutput file path: \"{output_file_path}\"")
-        json_data = []
-        start_time = time.time()
-
-        for file_name_ext in files:
-            file_name, file_ext = os.path.splitext(file_name_ext)
-            file_path = os.path.normpath(os.path.join(loc, file_name_ext))
-            image_content = tes.image_to_string(file_path, lang='ENG', config='--psm 10')
-
-            json_data.append({
-                "file_name": file_name,
-                "file_path": file_path,
-                "image_content": image_content,
-            })
-
-        with open(output_file_path, "w", encoding="utf8") as file:
-            json.dump(json_data, file, indent=4)
-        print(f'\tFinished in {time.time() - start_time:2.f} seconds')
+    print(f"All done in {time.time() - start_time:.2f} sec")
 
 
 if "__main__" == __name__:
